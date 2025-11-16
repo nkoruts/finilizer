4b5833fac26d356268fe7fcb08e6e2589e45baee
Nikita Koruts <nikitakoruts@gmail.com>
2025-11-16 21:44:25 +0200

    Add FileTreeReader

---
+++ Sources/FileTreeReader.swift
@@ -0,0 +1,27 @@
+//
+//  FileTreeReader.swift
+//  finilizer
+//
+//  Created by Nikita Koruts on 16.11.2025.
+//
+
+import Foundation
+
+struct FileTreeReader {
+    func readFromDirectory(with path: String) -> [URL] {
+        guard let urlPath = URL(string: path), FileManager.default.fileExists(atPath: path) else {
+            print("Directory not found at path: \(path)")
+            return []
+        }
+        guard let enumerator = FileManager.default.enumerator(at: urlPath, includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsHiddenFiles]),
+              !enumerator.allObjects.isEmpty
+        else {
+            print("Files not found in this path: \(path)")
+            return []
+        }
+        return enumerator.compactMap { element in
+            guard let url = element as? URL, url.pathExtension == "swift" else { return nil }
+            return url
+        }
+    }
+}
