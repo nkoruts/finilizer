c8095584841ce2c5fdcaf53d89bf1bbb23c48615
Nikita Koruts <nikitakoruts@gmail.com>
2025-11-17 09:19:02 +0200

    Fix code issues

--- Sources/ClassFinilizer.swift
+++ Sources/ClassFinilizer.swift
@@ -9,13 +9,12 @@ import SwiftParser
     
     static func main() {
         print("Enter the path to the root directory:")
-        guard let directoryPath = readLine(), !input.isEmpty else {
+        guard let directoryPath = readLine(), !directoryPath.isEmpty else {
             print("Path not entered. Finished.")
             exit(1)
         }
         
         let filePaths = FileTreeReader().readFromDirectory(with: directoryPath)
-        let visitor = ClassVisitor()
         let trees: [SourceFileSyntax] = filePaths.compactMap {
             guard let content = try? String(contentsOf: $0, encoding: .utf8) else {
                 print("File doesn't exist.")
@@ -23,8 +22,9 @@ import SwiftParser
             }
             return Parser.parse(source: content)
         }
-        visitor.walk(through: trees)
         
+        let visitor = ClassVisitor()
+        visitor.walk(through: trees)
         let finalizableClasses = visitor.finalizableClasses
         guard !finalizableClasses.isEmpty else { return }
         let rewriter = ClassRewriter(finilizableClasses: finalizableClasses)
